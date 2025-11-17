b4651907be35d9765edcc421e23761f919ed9220
cintia <ccastelgonzalez@safareyes.es>
2025-11-17 20:44:18 +0100

    ✨Creación de pantalla de inicio

---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  standalone: true,
+  imports: [CommonModule]
+})
+export class InicioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Código de inicialización
+  }
+}
