7d250e3a0e59b01db127e7d19a3469fce5e3c803
cintia <ccastelgonzalez@safareyes.es>
2025-11-26 21:36:05 +0100

    ♻️Actualización app routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import {InicioComponent} from './inicio/inicio.component';
+import {LoginComponent} from './login/login.component';
+import {RegistroComponent} from './registro/registro.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'inicio', component: InicioComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+
 
 
   // Opcional: ruta de "no encontrado"
   { path: '**', redirectTo: '/inicio' }
-
 ];
 
