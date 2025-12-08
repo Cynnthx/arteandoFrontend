19f3eca5db9962ccc56759aa2647bf14fd1d120e
cintia <ccastelgonzalez@safareyes.es>
2025-12-08 19:39:05 +0100

    ♻️ Actualización de las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,20 @@ import { Routes } from '@angular/router';
 import {InicioComponent} from './inicio/inicio.component';
 import {LoginComponent} from './login/login.component';
 import {RegistroComponent} from './registro/registro.component';
+import {PerfilComponent} from './perfil/perfil.component';
+import {TestsComponent} from './tests/tests.component';
+import {PreguntasComponent} from './preguntas/preguntas.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+
   { path: 'inicio', component: InicioComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'tests', component: TestsComponent },
+  { path: 'preguntas', component: PreguntasComponent },
+
 
 
 
