a333701271e5bcc4417b7adfe015d7ad555ad722
cintia <ccastelgonzalez@safareyes.es>
2026-01-18 19:26:08 +0100

    ♻️ Actualización de los componentes de tests

---
+++ src/app/tests/tests.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
+import {Tests} from '../modelos/tests';
+import {TestsServicio} from '../servicios/tests-servicio';
+
+
+
+@Component({
+  selector: 'app-tests',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './tests.component.html'
+})
+export class TestsComponent implements OnInit {
+
+  busqueda: string = '';
+  modalActivo: string | null = null;
+
+  tests: Tests[] = [];
+  testsFiltrados: Tests[] = [];
+
+  constructor(
+    private testsServicio: TestsServicio,
+    public router: Router
+  ) {}
+
+  ngOnInit() {
+    this.cargarTests();
+  }
+
+  cargarTests() {
+    this.testsServicio.listarTests().subscribe({
+      next: (data) => {
+        this.tests = data;
+        this.testsFiltrados = data;
+      },
+      error: () => {
+        alert('Error cargando los tests');
+      }
+    });
+  }
+
+  filtrarTests() {
+    const term = this.busqueda.toLowerCase();
+    this.testsFiltrados = this.tests.filter(test =>
+      test.titulo.toLowerCase().includes(term)
+    );
+  }
+
+  irAlTest(test: Tests) {
+    // aquí luego se puede ir al test por id
+    this.router.navigate(['/preguntas', test.id]);
+  }
+
+  abrirModal(tipo: string) {
+    this.modalActivo = tipo;
+  }
+
+  cerrarModal() {
+    this.modalActivo = null;
+  }
+}
