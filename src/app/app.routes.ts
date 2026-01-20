f85f224cec49927956bc5242477ee040b44a9156
cintia <ccastelgonzalez@safareyes.es>
2026-01-20 22:53:31 +0100

    ⭐ Se ha añadido nuevas rutas para pantalla de admin, dentro de test-admin hay preguntas modificables

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,17 +7,24 @@ import {TestsComponent} from './tests/tests.component';
 import {PreguntasComponent} from './preguntas/preguntas.component';
 import {TestAdminComponent} from './test-admin/test-admin.component';
 import {adminGuard} from './seguridad/admin.guard';
+import {PreguntaAdminComponent} from './pregunta-admin/pregunta-admin.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-
   { path: 'inicio', component: InicioComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'perfil', component: PerfilComponent },
   { path: 'tests', component: TestsComponent },
   { path: 'preguntas/:id', component: PreguntasComponent },
+
+// Rutas de administración protegidas
   { path: 'test-admin', component: TestAdminComponent, canActivate: [adminGuard] },
+  { path: 'test-admin/preguntas/:testId', component: PreguntaAdminComponent, canActivate: [adminGuard] },
+
+
+
+  { path: '**', redirectTo: '/inicio' },
 
 
 
